0a254aad665807bfb96fa811017bbbbacda8af2f
david m <david-michael@con-way.com>
2019-05-17 18:02:42 -0700

    Monday commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-app'`, () => {
+  it(`should have as title 'Test App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-app');
+    expect(app.title).toEqual('Test App');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Test App!');
   });
 });
